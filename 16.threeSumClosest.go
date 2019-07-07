64ca2a385ec98730b812894986e540ef06101e44
kwoktung <onlyif2009@gmail.com>
2019-07-07 21:47:57 +0800

    add threeSumCLosest

---
+++ 16.threeSumClosest.go
@@ -0,0 +1,35 @@
+package leetcode_go
+
+import "sort"
+
+func abs (n int) int {
+	if n < 0 { return -n }
+	return n
+}
+
+func threeSumClosest(nums []int, target int) int {
+	if len(nums) == 3 {
+		return nums[0] + nums[1] + nums[2]
+	}
+	sort.Ints(nums);
+	result := nums[0] + nums[1] + nums[len(nums) -1]
+	last := abs(nums[0] + nums[1] + nums[len(nums) -1] - target);
+	for i, l := 0, len(nums); i < l - 1; i++  {
+		left, right := i + 1, len(nums) - 1;
+		for left < right {
+			current := abs(nums[i] + nums[left] + nums[right] - target);
+			if current > last {
+				//break
+			} else {
+				last = current
+				result = nums[i] + nums[left] + nums[right]
+			}
+			if nums[i] + nums[left] + nums[right] - target > 0 {
+				right -= 1;
+			} else {
+				left += 1;
+			}
+		}
+	}
+	return result
+}
