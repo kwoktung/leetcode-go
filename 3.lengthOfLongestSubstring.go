f3dfd2154fc90c614565524da0dfa0bc6b1aaaed
kwoktung <onlyif2009@gmail.com>
2019-07-08 17:51:21 +0800

    add lengthOfLongestSubstring

---
+++ 3.lengthOfLongestSubstring.go
@@ -0,0 +1,17 @@
+package leetcode_go
+
+func lengthOfLongestSubstring(s string) int {
+	kv := make(map[byte] int)
+	max := 0
+	for start, end := 0, 0; end < len(s); end++  {
+		key := s[end];
+		if v, ok := kv[key]; ok && v >= start {
+			start = kv[key] + 1
+		}
+		if end - start + 1 > max {
+			max = end - start + 1
+		}
+		kv[key] = end
+	}
+	return max
+}
